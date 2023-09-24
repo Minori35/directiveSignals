c60d215519210c213dc1d345d5a1c98f95206978
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-09-24 15:25:13 -0600

    señales informacion usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { UserServicesService } from '../../services/user-services.service';
+import { User } from '../../interfaces/user-request.interface';
+import { first } from 'rxjs';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit{
+
+  constructor( private userServices : UserServicesService){} 
+  
+  public userId = signal(1);
+  
+  public currentUser= signal<User| undefined>(undefined);
+  
+  public UserWasFound = signal(true);
+  
+  ngOnInit(): void {
+    this.loaderUser(this.userId())
+  }
+
+  loaderUser(id : number){
+    if(id<=0) return
+
+    this.userId.set(id) 
+
+    this.userServices.getByUse(id).
+      subscribe(user => {
+        this.currentUser.set(user)
+        
+      })
+  }
+
+
 
 }
